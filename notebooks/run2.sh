91decf5498b885fb3ae16e118af430f2fc9b6da9
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2023-05-18 15:28:32 +0000

    delete scripts

--- notebooks/run2.sh
+++
@@ -1,2 +0,0 @@
-python run_treesatai.py --root /workspace/storage/data/treesatai --batch-size 128 --workers 128 --seed 42 --device cuda:1 --faiss
-python run_treesatai.py --root /workspace/storage/data/treesatai --batch-size 128 --workers 128 --seed 2023 --device cuda:1 --faiss
